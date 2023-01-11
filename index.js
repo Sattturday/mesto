d5b0456862bf40b57cecf99986a1dcecc3cf33c0
Alla Magina <babara@flylady.su>
2023-01-11 13:43:46 +0300

    feat: сделать плейсхолдеры перезаписываемыми

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 let content = document.querySelector('.content');
-let popup = content.querySelector('.popup');
-let profileEditButton = content.querySelector('.profile__edit-button');
-let popupButtonClose = popup.querySelector('.popup__close');
+let popup = content.querySelector('.popup'); // Находим попап
+let profileEditButton = content.querySelector('.profile__edit-button'); // Находим кнопку редактирования профиля
+let popupButtonClose = popup.querySelector('.popup__close'); // Нахоидим кнопку закрытия формы
+let popupForm = popup.querySelector('.popup__container'); // Находим форму в DOM
 
 function popupOpened(event) {
-  event.preventDefault();
-  popup.classList.toggle('popup_opened');
+  event.preventDefault(); // Отменяем поведение браузера по умолчанию
+  popup.classList.toggle('popup_opened'); // Добавляем/удаляем класс popup_opened
+  popupForm.reset(); // Сбрасываем форму
 }
 
-profileEditButton.addEventListener('click', popupOpened);
-popupButtonClose.addEventListener('click', popupOpened);
-
-// Находим форму в DOM
-let popupForm = popup.querySelector('.popup__container');
+profileEditButton.addEventListener('click', popupOpened); // Клик кнопки редактирования профиля
+popupButtonClose.addEventListener('click', popupOpened); // Клик кнопки закрытия формы
 
 // Находим поля формы в DOM
 let nameInput = popupForm.querySelector('.popup__input_name');
@@ -31,12 +30,15 @@ function handleFormSubmit (event) {
     let profileName = content.querySelector('.profile__info-name');
     let profileJob = content.querySelector('.profile__info-job');
 
+    // Меняем значение атрибутов placeholder
+    nameInput.setAttribute('placeholder', name);
+    jobInput.setAttribute('placeholder', job);
+
     // Вставляем новые значения с помощью textContent
     profileName.textContent = name;
     profileJob.textContent = job;
 
-    // Закрываем попап
-    popup.classList.toggle('popup_opened');
+    popup.classList.toggle('popup_opened'); // Закрываем попап, удаляя класс popup_opened
 }
 
 // Прикрепляем обработчик к форме:
