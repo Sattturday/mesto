cbe4348807e3af375cfb6c506ee09829f3d8b841
Alla Magina <babara@flylady.su>
2023-02-01 14:42:46 +0300

    refactor: поменять переменные на константы

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
-let content = document.querySelector('.content');
-let popup = content.querySelector('.popup'); // Находим попап
-let profileEditButton = content.querySelector('.profile__button_edit'); // Находим кнопку редактирования профиля
-let popupButtonClose = popup.querySelector('.popup__close'); // Нахоидим кнопку закрытия формы
-let popupForm = popup.querySelector('.popup__container'); // Находим форму в DOM
-let nameInput = popupForm.querySelector('.popup__input_name'); // Находим поля формы в DOM
-let jobInput = popupForm.querySelector('.popup__input_job'); // Находим поля формы в DOM
+const content = document.querySelector('.content');
+const popup = content.querySelector('.popup'); // Находим попап
+const popupForm = popup.querySelector('.popup__container'); // Находим форму в DOM
 
 function popupStateChange() {
   popup.classList.toggle('popup_opened'); // Добавляем/удаляем класс popup_opened
   popupForm.reset(); // Сбрасываем форму
 }
+
+const profileEditButton = content.querySelector('.profile__button_edit'); // Находим кнопку редактирования профиля
+const popupButtonClose = popup.querySelector('.popup__close'); // Нахоидим кнопку закрытия формы
+
+profileEditButton.addEventListener('click', popupStateChange); // Клик кнопки редактирования профиля
+popupButtonClose.addEventListener('click', popupStateChange); // Клик кнопки закрытия формы
 
 //закрытие попапа по клику на оверлей
 const handleOverlayClick = (event) => {
@@ -18,12 +20,12 @@ const handleOverlayClick = (event) => {
   }
 }
 
-profileEditButton.addEventListener('click', popupStateChange); // Клик кнопки редактирования профиля
-popupButtonClose.addEventListener('click', popupStateChange); // Клик кнопки закрытия формы
 popup.addEventListener('click', handleOverlayClick); // Клик по оверлею
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+const nameInput = popupForm.querySelector('.popup__input_name'); // Находим поля формы в DOM
+const jobInput = popupForm.querySelector('.popup__input_job'); // Находим поля формы в DOM
+
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
@@ -32,8 +34,8 @@ function handleFormSubmit(evt) {
   let job = jobInput.value;
 
   // Выбераем элементы, куда должны быть вставлены значения полей
-  let profileName = content.querySelector('.profile__info-name');
-  let profileJob = content.querySelector('.profile__info-job');
+  const profileName = content.querySelector('.profile__info-name');
+  const profileJob = content.querySelector('.profile__info-job');
 
   // Вставляем новые значения с помощью textContent
   profileName.textContent = name;
