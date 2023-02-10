d61b2ac557bc8a7666ca1f02e00d832c0264ae0c
Alla Magina <babara@flylady.su>
2023-02-10 13:22:00 +0300

    feat: реализовать отрисовку карточек при закрузге страницы, плавное всплытие попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,50 @@
+const initialCards = [
+  {
+    name: 'Камчатка',
+    link: './images/cards-kamchatka.jpg'
+  },
+  {
+    name: 'Белорецк',
+    link: './images/cards-beloretsk.jpg'
+  },
+  {
+    name: 'Урал',
+    link: './images/cards-ural.jpg'
+  },
+  {
+    name: 'Покров',
+    link: './images/cards-pokrov.jpg'
+  },
+  {
+    name: 'Летний Байкал',
+    link: './images/cards-baikal-leto.jpg'
+  },
+  {
+    name: 'Байкал заледенел',
+    link: './images/cards-baikal-zima.jpg'
+  }
+];
+
 const content = document.querySelector('.content');
+const cardsList = content.querySelector('.cards');
+const cardTemplate = document.querySelector('.card-template').content; // Находим содержимое темплейта с карточкий
+
+initialCards.forEach(function (element) {
+  const cardElement = cardTemplate.querySelector('.cards__item').cloneNode(true); // Клонируем содержимое
+
+  cardElement.querySelector('.cards__title').textContent = element.name; // Вписываем название карточки
+
+  const cardImage = cardElement.querySelector('.cards__image'); // Находим имг
+  cardImage.setAttribute('src', element.link); // Вставляем ссылку на изображение
+  cardImage.setAttribute('alt', element.name); // Вставляем значение атрибута alt
+
+  cardElement.querySelector('.cards__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('cards__like_active');
+  });
+
+  cardsList.append(cardElement);
+})
+
 const popup = content.querySelector('.popup'); // Находим попап
 const popupForm = popup.querySelector('.popup__container'); // Находим форму в DOM
 
