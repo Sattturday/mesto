a1455bb5823347811e1277f923aae51dc90aeb52
Alla Magina <babara@flylady.su>
2023-02-11 15:34:33 +0300

    feat: реализовать добавление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -46,36 +46,35 @@ initialCards.forEach((element) => {
 })
 
 
+// Попап редактирования профиля
+const popupEdit = content.querySelector('.popup_for_edit-profile'); // Находим попап редактирования
+const popupForm = popupEdit.querySelector('.popup__container'); // Находим форму
 
-const popup = content.querySelector('.popup'); // Находим попап
-const popupForm = popup.querySelector('.popup__container'); // Находим форму в DOM
-
-const changePopupState = () => {
-  popup.classList.toggle('popup_opened'); // Добавляем/удаляем класс popup_opened
+const changePopupEditState = () => {
+  popupEdit.classList.toggle('popup_opened'); // Добавляем/удаляем класс popup_opened
   popupForm.reset() // Сбрасываем форму
 }
 
 const profileEditButton = content.querySelector('.profile__button_edit'); // Находим кнопку редактирования профиля
-const addButton = content.querySelector('.profile__button_add'); // Находим кнопку добавления карточки
-const popupButtonClose = popup.querySelector('.popup__close'); // Нахоидим кнопку закрытия формы
+const popupButtonClose = popupEdit.querySelector('.popup__close'); // Нахоидим кнопку закрытия формы
 
-profileEditButton.addEventListener('click', changePopupState); // Клик кнопки редактирования профиля
-popupButtonClose.addEventListener('click', changePopupState); // Клик кнопки закрытия формы
+profileEditButton.addEventListener('click', changePopupEditState); // Клик кнопки редактирования профиля
+popupButtonClose.addEventListener('click', changePopupEditState); // Клик кнопки закрытия формы
 
 //закрытие попапа по клику на оверлей
 const handleOverlayClick = (event) => {
   if (event.target === event.currentTarget) {
-    changePopupState();
+    changePopupEditState();
   }
 }
 
-popup.addEventListener('click', handleOverlayClick); // Клик по оверлею
+popupEdit.addEventListener('click', handleOverlayClick); // Клик по оверлею
 
 const nameInput = popupForm.querySelector('.popup__input_name'); // Находим поля формы в DOM
 const jobInput = popupForm.querySelector('.popup__input_job'); // Находим поля формы в DOM
 
-// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
-const handleFormEditProfileSubmit = (evt) => {
+// Обработчик «отправки» формы редактирования профиля
+const handleFormEditSubmit = (evt) => {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
   // Получаем значение полей jobInput и nameInput из свойства value
@@ -95,8 +94,69 @@ const handleFormEditProfileSubmit = (evt) => {
   jobInput.setAttribute('value', job);
 
   // Закрываем попап
-  changePopupState();
+  changePopupEditState();
 }
 
-popupForm.addEventListener('submit', handleFormEditProfileSubmit); // Прикрепляем обработчик к форме
+popupForm.addEventListener('submit', handleFormEditSubmit); // Прикрепляем обработчик к форме
 
+
+
+// Теперь попап добавления карточки
+const popupAdd = content.querySelector('.popup_for_add-card'); // Находим попап добавления
+const popupAddForm = popupAdd.querySelector('.popup__container'); // Находим форму добавления
+
+const changePopupAddState = () => {
+  popupAdd.classList.toggle('popup_opened');
+  popupAddForm.reset()
+}
+
+const addButton = content.querySelector('.profile__button_add'); // Находим кнопку добавления карточки
+const popupAddButtonClose = popupAdd.querySelector('.popup__close'); // Нахоидим кнопку закрытия формы
+
+addButton.addEventListener('click', changePopupAddState);
+popupAddButtonClose.addEventListener('click', changePopupAddState);
+
+const handleOverlayAddClick = (event) => {
+  if (event.target === event.currentTarget) {
+    changePopupAddState();
+  }
+}
+
+popupAdd.addEventListener('click', handleOverlayAddClick); // Клик пооверлею
+
+// Находим поля формы
+const cardName = popupAddForm.querySelector('.popup__input_card-name');
+const cardLinkImage = popupAddForm.querySelector('.popup__input_card-link-image');
+
+// Обработчик «отправки» формы создания карточки
+const handleFormAddSubmit = (evt) => {
+  evt.preventDefault();
+
+  // Получаем значение полей cardName и cardLinkImage из свойства value
+  let name = cardName.value;
+  let link = cardLinkImage.value;
+
+  // Готовим карточку
+  const newCard = cardTemplate.querySelector('.cards__item').cloneNode(true); // Клонируем содержимое template
+
+  newCard.querySelector('.cards__title').textContent = name; // Вписываем название карточки
+
+  const cardImage = newCard.querySelector('.cards__image'); // Находим имг
+  cardImage.setAttribute('src', link); // Вставляем ссылку на изображение
+  cardImage.setAttribute('alt', name); // Вставляем значение атрибута alt
+
+  newCard.querySelector('.cards__like').addEventListener('click', (evt) => {
+    evt.target.classList.toggle('cards__like_active');
+  });
+
+  // Вставляем карточку
+  cardsList.prepend(newCard);
+
+  // Закрываем попап
+  changePopupAddState();
+}
+
+popupAddForm.addEventListener('submit', handleFormAddSubmit); // Прикрепляем обработчик к форме
+
+
+// Теперь удаление карточек
