dc80520bd04025d84e693c41a9b862198463dffe
Alla Magina <babara@flylady.su>
2023-02-13 12:15:30 +0300

    refact: сделать функцию создания карточки со всеми слушателями

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,4 @@
-/*const initialCards = [
+const initialCards = [
   {
     name: 'Камчатка',
     link: './images/cards-kamchatka.jpg'
@@ -24,28 +24,29 @@
     link: './images/cards-baikal-zima.jpg'
   }
 ];
-*/
-const content = document.querySelector('.content');
-//const cardsList = document.querySelector('.cards');
-//const cardTemplate = document.querySelector('.card-template').content; // находим содержимое темплейта с карточкий
+
+const cardsList = document.querySelector('.cards');
+const cardTemplate = document.querySelector('.card-template').content; // находим содержимое темплейта с карточкий
 
 const popups = document.querySelectorAll('.popup'); // находим все попапы
 
-const popupProfile = content.querySelector('.popup_for_edit-profile'); // находим попап профиля
-const profileEditButton = content.querySelector('.profile__button_edit'); // находим кнопку редактирования профиля
+const popupProfile = document.querySelector('.popup_for_edit-profile'); // находим попап профиля
+const profileEditButton = document.querySelector('.profile__button_edit'); // находим кнопку редактирования профиля
 
 const popupAddCard = document.querySelector('.popup_for_add-card'); // попап добавления карточки
 const cardAddButton = document.querySelector('.profile__button_add'); // находим кнопку добавления карточки
 
 const popupImage = document.querySelector('.popup_for_full-image'); // попап с картинкой
+const fullImage = popupImage.querySelector('.popup__full-image'); // image in popup
+const fullImageCaption = popupImage.querySelector('.popup__caption'); // caption in popup
 
-const closeButtons = document.querySelectorAll('.popup__close'); // находим все крестики проекта по универсальному селектору
 
 profileEditButton.addEventListener('click', () => popupProfile.classList.add('popup_opened')); // открытие попапа профиля
 cardAddButton.addEventListener('click', () => popupAddCard.classList.add('popup_opened')); // открытие попапа добавления карточки
 
+// закрытие попапа
 const closePopup = (popup) => {
-  popup.classList.remove('popup_opened');  // закрытие попапа
+  popup.classList.remove('popup_opened');
 }
 
 popups.forEach((popup) => {
@@ -58,16 +59,17 @@ popups.forEach((popup) => {
     }
   })
 })
-/*
-closeButtons.forEach((button) => {
-  // находим 1 раз ближайший к крестику попап
-  const popup = button.closest('.popup');
-  // устанавливаем обработчик закрытия на крестик
-  button.addEventListener('click', () => closePopup(popup));
-});
 
-/*
+// редактирование попапа с картинкой
+function handleCardClick(link, name) {
+  fullImage.setAttribute('src', link); // Вставляем ссылку на изображение
+  fullImage.setAttribute('alt', name); // Прописываем изображению alt
+  fullImageCaption.textContent = name; // Вставляем значение атрибута alt
 
+  popupImage.classList.add('popup_opened');
+}
+
+// создаем карточку
 const createCard = (element) => {
   const cardElement = cardTemplate.querySelector('.cards__item').cloneNode(true); // Клонируем содержимое
   cardElement.querySelector('.cards__title').textContent = element.name; // Вписываем название карточки
@@ -76,24 +78,30 @@ const createCard = (element) => {
   cardImage.setAttribute('src', element.link); // Вставляем ссылку на изображение
   cardImage.setAttribute('alt', element.name); // Вставляем значение атрибута alt
 
+  // просмотр полного изображения
+  cardImage.addEventListener('click', () => handleCardClick(element.link, element.name));
 
+  // лайки
   cardElement.querySelector('.cards__like').addEventListener('click', (evt) => {
     evt.target.classList.toggle('cards__like_active');
   });
 
+  // удаление карточек
   cardElement.querySelector('.cards__delete').addEventListener('click', (evt) => {
-    evt.target.parentElement.remove();
+    const card = evt.target.closest('.cards__item');
+    card.remove();
   });
 
   return cardElement;
 }
 
+// вставляем карточки при загрузке
 initialCards.forEach((element) => {
   const cardElement = createCard(element)
   cardsList.append(cardElement);
 })
 
 
-*/
+
 
 
