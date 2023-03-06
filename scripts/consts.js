60a99c0070eaf64ba99344073e06f845e904a90b
Alla Magina <babara@flylady.su>
2023-03-07 00:27:05 +0300

    fix: исправить комментарии в consts.js

--- scripts/consts.js
+++ scripts/consts.js
@@ -35,26 +35,26 @@ const validationOptions = {
   errorClass: 'popup__error_visible', // show mistakes
 };
 
-const popups = document.querySelectorAll('.popup'); // находим все попапы
+const popups = document.querySelectorAll('.popup'); // все попапы
 
-const profileEditButton = document.querySelector('.profile__button_edit'); // находим кнопку редактирования профиля
-const popupProfile = document.querySelector('.popup_for_edit-profile'); // находим попап профиля
-const profileName = document.querySelector('.profile__info-name'); // находим имя в профиле
-const profileJob = document.querySelector('.profile__info-job'); // находим род занятий в профиле
-const profileForm = popupProfile.querySelector('.popup__container'); // Находим форму профиля
-const inputProfileName = profileForm.querySelector('.popup__input_name'); // Находим поля формы профиля
-const inputProfileJob = profileForm.querySelector('.popup__input_job'); // Находим поля формы профиля
+const profileEditButton = document.querySelector('.profile__button_edit'); // кнопкa ред. профиля
+const popupProfile = document.querySelector('.popup_for_edit-profile'); // попап профиля
+const profileName = document.querySelector('.profile__info-name'); // имя в профиле
+const profileJob = document.querySelector('.profile__info-job'); // род занятий в профиле
+const profileForm = popupProfile.querySelector('.popup__container'); // формa профиля
+const inputProfileName = profileForm.querySelector('.popup__input_name'); // поля формы профиля
+const inputProfileJob = profileForm.querySelector('.popup__input_job'); // поля формы профиля
 
 const popupAddCard = document.querySelector('.popup_for_add-card'); // попап добавления карточки
-const addCardButton = document.querySelector('.profile__button_add'); // находим кнопку добавления карточки
-const addCardForm = popupAddCard.querySelector('.popup__container'); // форма добавления карточек
-const inputAddCardName = popupAddCard.querySelector('.popup__input_card-name'); // поле для имени карточки
+const addCardButton = document.querySelector('.profile__button_add'); // кнопкa добавления карточки
+const addCardForm = popupAddCard.querySelector('.popup__container'); // добавления карточек
+const inputAddCardName = popupAddCard.querySelector('.popup__input_card-name'); // поле имени карточки
 const inputAddCardLink = popupAddCard.querySelector(
   '.popup__input_card-link-image'
-); // поле для ссылки на изображение
+); // поле для ссылки
 
 const cardsList = document.querySelector('.cards'); // место для карточек
-const cardTemplate = document.querySelector('.card-template').content; // находим содержимое темплейта с карточкий
+const cardTemplate = document.querySelector('.card-template').content; // темплейт с карточкий
 const popupImage = document.querySelector('.popup_for_full-image'); // попап с картинкой
 const fullImage = popupImage.querySelector('.popup__full-image'); // image in popup
 const fullImageCaption = popupImage.querySelector('.popup__caption'); // caption in popup
