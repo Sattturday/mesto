3170d1f4e16e2a4199ef82139c65c5d87d266082
Alla Magina <babara@flylady.su>
2023-03-06 17:47:56 +0300

    fix: сделать кнопку активной при открытии попапа профиля

--- scripts/consts.js
+++ scripts/consts.js
@@ -34,3 +34,27 @@ const validationOptions = {
   inputErrorClass: 'popup__input_type_error', // red border-bottom
   errorClass: 'popup__error_visible', // show mistakes
 };
+
+const popups = document.querySelectorAll('.popup'); // находим все попапы
+
+const profileEditButton = document.querySelector('.profile__button_edit'); // находим кнопку редактирования профиля
+const popupProfile = document.querySelector('.popup_for_edit-profile'); // находим попап профиля
+const profileName = document.querySelector('.profile__info-name'); // находим имя в профиле
+const profileJob = document.querySelector('.profile__info-job'); // находим род занятий в профиле
+const profileForm = popupProfile.querySelector('.popup__container'); // Находим форму профиля
+const inputProfileName = profileForm.querySelector('.popup__input_name'); // Находим поля формы профиля
+const inputProfileJob = profileForm.querySelector('.popup__input_job'); // Находим поля формы профиля
+
+const popupAddCard = document.querySelector('.popup_for_add-card'); // попап добавления карточки
+const addCardButton = document.querySelector('.profile__button_add'); // находим кнопку добавления карточки
+const addCardForm = popupAddCard.querySelector('.popup__container'); // форма добавления карточек
+const inputAddCardName = popupAddCard.querySelector('.popup__input_card-name'); // поле для имени карточки
+const inputAddCardLink = popupAddCard.querySelector(
+  '.popup__input_card-link-image'
+); // поле для ссылки на изображение
+
+const cardsList = document.querySelector('.cards'); // место для карточек
+const cardTemplate = document.querySelector('.card-template').content; // находим содержимое темплейта с карточкий
+const popupImage = document.querySelector('.popup_for_full-image'); // попап с картинкой
+const fullImage = popupImage.querySelector('.popup__full-image'); // image in popup
+const fullImageCaption = popupImage.querySelector('.popup__caption'); // caption in popup
--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,3 @@
-const popups = document.querySelectorAll('.popup'); // находим все попапы
-
-const profileEditButton = document.querySelector('.profile__button_edit'); // находим кнопку редактирования профиля
-const popupProfile = document.querySelector('.popup_for_edit-profile'); // находим попап профиля
-const profileName = document.querySelector('.profile__info-name'); // находим имя в профиле
-const profileJob = document.querySelector('.profile__info-job'); // находим род занятий в профиле
-const profileForm = popupProfile.querySelector('.popup__container'); // Находим форму профиля
-const inputProfileName = profileForm.querySelector('.popup__input_name'); // Находим поля формы профиля
-const inputProfileJob = profileForm.querySelector('.popup__input_job'); // Находим поля формы профиля
-
-const popupAddCard = document.querySelector('.popup_for_add-card'); // попап добавления карточки
-const addCardButton = document.querySelector('.profile__button_add'); // находим кнопку добавления карточки
-const addCardForm = popupAddCard.querySelector('.popup__container'); // форма добавления карточек
-const inputAddCardName = popupAddCard.querySelector('.popup__input_card-name'); // поле для имени карточки
-const inputAddCardLink = popupAddCard.querySelector(
-  '.popup__input_card-link-image'
-); // поле для ссылки на изображение
-
-const cardsList = document.querySelector('.cards'); // место для карточек
-const cardTemplate = document.querySelector('.card-template').content; // находим содержимое темплейта с карточкий
-const popupImage = document.querySelector('.popup_for_full-image'); // попап с картинкой
-const fullImage = popupImage.querySelector('.popup__full-image'); // image in popup
-const fullImageCaption = popupImage.querySelector('.popup__caption'); // caption in popup
-
 const closeByEscape = (evt) => {
   if (evt.key === 'Escape') {
     const openedPopup = document.querySelector('.popup_opened');
@@ -107,6 +83,7 @@ initialCards.forEach((element) => {
   cardsList.append(cardElement);
 });
 
+// добавление новых карточек
 const handleAddCardSubmit = (evt) => {
   evt.preventDefault();
 
@@ -118,6 +95,7 @@ const handleAddCardSubmit = (evt) => {
   const cardElement = createCard(element);
   cardsList.prepend(cardElement);
 
+  addCardForm.reset();
   closePopup(popupAddCard);
 };
 
@@ -137,4 +115,4 @@ const handleFormEditSubmit = (evt) => {
 
 profileForm.addEventListener('submit', handleFormEditSubmit); // Прикрепляем обработчик к форме
 
-// enableValidation(validationOptions);
+enableValidation(validationOptions);
